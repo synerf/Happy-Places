bc32a3bb13094950b66fc38d8327ea4b39bc86f8
Ashish Pimpre <50199708+synerf@users.noreply.github.com>
2021-12-17 01:10:20 +0530

    add AddHappyPlaceActivity screen, setup Dexter for permissions

--- app/src/main/java/com/synerf/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/synerf/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,133 @@
 package com.synerf.happyplaces
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.synerf.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityAddHappyPlaceBinding
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarAddPlace)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+                _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        binding.etDate.setOnClickListener(this)
+        binding.tvAddImage.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.etDate -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            R.id.tvAddImage -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf(
+                    "Select photo from gallery", "Capture photo from camera"
+                )
+                pictureDialog.setItems(pictureDialogItems) {
+                    _, which ->
+                    when(which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "Camera selection coming soon...",
+                        Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }.show()
+            }
+        }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(cal.time).toString())
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        ).withListener(object: MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "Storage READ/WRITE permissions granted. Now you can select image from the gallery",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off " +
+                "permissions required for this feature. It can be enabled under application" +
+                "settings.")
+            .setPositiveButton("GO TO SETTINGS") {
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
--- app/src/main/java/com/synerf/happyplaces/MainActivity.kt
+++ app/src/main/java/com/synerf/happyplaces/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.synerf.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.synerf.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
